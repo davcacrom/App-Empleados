521531bf0112a81cd5026df2c38504720e78ec7f
Núñez Martínez <dnunezm@minsait.com>
2022-12-02 10:31:56 +0100

    Modelos

---
+++ src/app/usuario/modelo/usuario.interface.ts
@@ -0,0 +1,6 @@
+export interface Usuario {
+
+  username:string;
+  e_mail:string;
+  password:string;
+}
--- src/app/usuario/usuario-service.service.ts
+++ src/app/usuario/usuario-service.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Usuario } from './modelo/usuario.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioServiceService {
 
+  usuarios:Usuario[] = [
+    {
+      username:"admin",
+      e_mail:"dcaceres@gmail.com",
+      password: "admin"
+    }]
   constructor() { }
 }
